1584df32da7d9e34163ee106c855c07c20127684
Suman Karmakar <karmakarsuman12138@gmail.com>
2026-06-30 23:09:30 +0530

    add

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -8,7 +8,7 @@ exports.getWorkouts= async(req, res) => {
 
     const user_id= req.user._id
 
-    const workouts = await orkout.find({user_id}).sort({createdAt: -1})
+    const workouts = await Workout.find({user_id}).sort({createdAt: -1})
 
     if(!workouts){
         return res.status(400).json({error: "No entries found"})
